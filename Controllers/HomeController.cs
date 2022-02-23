cbaede8dd01c4231546b2831efea2d2bdf06f726
Payam7682 <payam7682@yahoo.com>
2022-02-24 10:50:31 +1300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Api.MvcSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Polly;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace UI.MvcSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<bool> IndexAsync([FromForm] UserModel model)
+        {
+            var policy = Policy.Handle<Exception>().Retry();
+            try
+            {
+                using var client = new HttpClient();
+                client.BaseAddress = new Uri("https://localhost:44393/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method
+                var response = policy.ExecuteAndCapture(() => client.PostAsJsonAsync("https://localhost:44332/api/dataoperation", model).Result);
+                return response.Result.IsSuccessStatusCode;                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
